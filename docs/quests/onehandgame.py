039c0b988862d34862c3e94230b6724179d704f6
scvgong <gongmyeongyun@gmail.com>
2023-12-04 16:25:48 +0900

    modifiy onehandgame

--- docs/quests/onehandgame.py
+++ docs/quests/onehandgame.py
@@ -4,25 +4,26 @@ three = "2"
 four = "0"
 five = "3"
 
+# 1,0,2,0,3
 print("{}, {}, {}, {}, {}.".format(one, two, three, four, five))
 
-one, two = two, one # 1, 0 -> 0, 1 로 변환
+one, two = two, one # 1, 0 -> 0, 1 로 변환, 0,1,2,0,3
 print("{}, {}, {}, {}, {}.".format(one, two, three, four, five))
 
-two, four = four, two # 0, 1 -> 1, 0 로 변환
+two, three = three, two # 2, 0 -> 2, 0 로 변환 0,2,1,0,3
 print("{}, {}, {}, {}, {}.".format(one, two, three, four, five))
 
-two, three = three, two # 2, 0 -> 2, 0 로 변환
+three, four = four , three # 1, 0 -> 0, 1 로 변환 0,2,0,1,3
 print("{}, {}, {}, {}, {}.".format(one, two, three, four, five))
 
-five, four = four, five
+five, four = four, five # 1, 3 -> 3, 1 로 변환 0,2,0,3,1
 print("{}, {}, {}, {}, {}.".format(one, two, three, four, five))
 
-three, four = four, three
+three, four = four, three # 3, 0 -> 0, 3로 변환 0,2,3,0,1
 print("{}, {}, {}, {}, {}.".format(one, two, three, four, five))
 
-two, three = three, two
+two, three = three, two # 3, 2 -> 2, 3 로 변환 0,3,2,0,1
 print("{}, {}, {}, {}, {}.".format(one, two, three, four, five))
 
-one, two = two, one
+one, two = two, one # 0, 3 -> 3, 0 로 변환 3,0,2,0,1
 print("{}, {}, {}, {}, {}.".format(one, two, three, four, five))
