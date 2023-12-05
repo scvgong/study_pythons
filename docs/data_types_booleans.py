3c6980768ff889cb645fb9d5c0a20b511d09ec50
scvgong <gongmyeongyun@gmail.com>
2023-12-05 15:00:03 +0900

    add if_elses

---
+++ docs/data_types_booleans.py
@@ -0,0 +1,20 @@
+bool_flag = True # yes와 동일
+pass
+
+bool_flag = False
+type(bool_flag)
+# <class 'bool'>
+
+# 문자 판단
+# 같음에 대한 기호 '=='
+str_name = "myeongyun gong"  
+str_name == "myeongyun gong"
+# True
+str_name == "myeongyungong"
+# False
+
+## 같지 않음 기호
+str_name != "myeongyungong"
+# True
+str_name != "myeongyun gong"
+# False
---
+++ docs/if_elses.py
@@ -0,0 +1,23 @@
+# 기본 if - elif - else 구문 :과 tab의 조화
+if True :
+    pass
+elif True :
+    pass
+else :
+    pass
+
+# 문자 비교
+str_name = "myeongyun gong"
+
+# 질문 형식(condition) = 변수 + 부등호 + 기준값
+# 문자에 대한 긍정으로 질문
+if str_name == "myeongyun gong" :
+    pass
+    print("name is {}.".format(str_name))
+
+# 문자에 대한 부정으로 질문
+if str_name != "myeongyun gong" :
+    pass
+    print("name is not {}.".format(str_name))
+
+print("End Program")
