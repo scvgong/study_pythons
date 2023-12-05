4e0b0d121f8fe1e1cc476849f4ef41eefbe8026a
scvgong <gongmyeongyun@gmail.com>
2023-12-05 17:49:24 +0900

    modifiy timestables

--- docs/quests/timestables.py
+++ docs/quests/timestables.py
@@ -1,8 +1,21 @@
 # 단수 입력 받아 연산, 구구단
-# - 구구단 5단 단계별 표시(while 문 사용)
+## - 구구단 5단 단계별 표시(while 문 사용)
 # 예. 5 * 1= 5  5 * 2 = 10 ... 5 * 9 = 45
-# - option) 단수 입력 받아 연산
+## - option) 단수 입력 받아 연산
 
+# 구구단 5단 연산
+gugu = 5
+num = 0
+
+while num < 9:
+    pass
+    num = num + 1
+    result = int(gugu) * int(num)
+    print("{} * {} = {}".format(gugu,num,result))
+    pass
+print("End Program")
+
+# 단수 입력 받아 연산ß
 gugu = int(input("구구단 숫자를 입력하세요 : "))
 num = 0
 
