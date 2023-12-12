e1f0d9af580c0cc24518eabd89be2b68f0ab00bb
scvgong <gongmyeongyun@gmail.com>
2023-12-12 18:25:14 +0900

    modifiy

--- docs/quests/data_type_questions_mix.py
+++ docs/quests/data_type_questions_mix.py
@@ -1,28 +1,31 @@
 
-mixed_question = [] # 최종
-dict_question = {} # 질문
+def question():
+    mixed_question = [] # 최종
+    dict_question = {} # 질문
 
-# 문제 입력
-for i in range(3):
-    question = input("question : ")
-    dict_question['question'] = question
-     # 답변 입력
-    list_answers = []
-    for x in range(4):
-        answer = input("answer : ")
-        list_answers.append(answer)
+    # 문제 입력
+    for i in range(3):
+        question = input("question : ")
+        dict_question['question'] = question
+        # 답변 입력
+        list_answers = []
+        for x in range(4):
+            answer = input("answer : ")
+            list_answers.append(answer)
+            pass
         pass
-    pass
-        
-        
-    correct = int(input("correct_index : "))
-    score = int(input("score : "))
+            
+            
+        correct = int(input("correct_index : "))
+        score = int(input("score : "))
 
-    dict_question['answer'] = list_answers
-    dict_question['correct_index'] = correct
-    dict_question['socre'] = score
+        dict_question['answer'] = list_answers
+        dict_question['correct_index'] = correct
+        dict_question['socre'] = score
 
-        
-    mixed_question.append(dict_question.copy())
+            
+        mixed_question.append(dict_question.copy())
+    return mixed_question
 
-print(mixed_question)
+result = question()
+print(result)
